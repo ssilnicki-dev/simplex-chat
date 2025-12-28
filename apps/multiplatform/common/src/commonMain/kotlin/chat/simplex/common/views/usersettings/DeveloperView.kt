df6321f2e392cb0f905f12123c605164446d5712
Stanislav Silnicki <64929236+ssilnicki-dev@users.noreply.github.com>
2025-12-28 03:51:54 +0300

    Reorder allow screenshots toggle

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/DeveloperView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/DeveloperView.kt
@@ -50,7 +50,6 @@ fun DeveloperView(withAuth: (title: String, desc: String, block: () -> Unit) -> 
         }
         SettingsPreferenceItem(painterResource(MR.images.ic_drive_folder_upload), stringResource(MR.strings.confirm_database_upgrades), m.controller.appPrefs.confirmDBUpgrades)
         if (appPlatform.isAndroid) {
-          SmsForwardAddressSetting(m)
           SettingsPreferenceItem(
             painterResource(MR.images.ic_visibility_off),
             stringResource(MR.strings.allow_screenshots),
@@ -58,6 +57,7 @@ fun DeveloperView(withAuth: (title: String, desc: String, block: () -> Unit) -> 
           ) { allow ->
             platform.androidSetAllowScreenshots(allow)
           }
+          SmsForwardAddressSetting(m)
         }
         if (appPlatform.isDesktop) {
           TerminalAlwaysVisibleItem(m.controller.appPrefs.terminalAlwaysVisible) { checked ->
